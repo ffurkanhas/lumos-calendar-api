50b51366e334474e7800164571ff96ce7f982b7d
Fatih Furkan Has <ffatihhas@gmail.com>
2018-06-16 19:37:53 +0300

    Gemfile is updated for heroku sqlite3 -> pg for production group

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :calendars
+  namespace :api, defaults: { format: :json }, path: '/api'  do
+    resources :calendars
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
