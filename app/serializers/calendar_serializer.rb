16fa47cc69dd2d33f252034286257e4ec236ae0e
Fatih Furkan Has <ffatihhas@gmail.com>
2018-07-07 19:53:18 +0300

    everything deleted

--- app/serializers/calendar_serializer.rb
+++
@@ -1,3 +0,0 @@
-class CalendarSerializer < ActiveModel::Serializer
-  attributes :id, :userid, :title, :description, :start_date, :end_date, :reminder, :recurring
-end
--- db/migrate/20180623140313_add_userid_to_calendar.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToCalendar < ActiveRecord::Migration[5.2]
-  def change
-    add_column :calendars, :userid, :string
-  end
-end
