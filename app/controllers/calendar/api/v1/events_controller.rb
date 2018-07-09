1a362170749639ed0128c3466b432897721c6221
Benan Bardak <benanbardak6@gmail.com>
2018-07-09 11:49:27 +0300

    versioning is added\nsearch with keyword functin is added

---
+++ app/controllers/calendar/api/v1/events_controller.rb
@@ -0,0 +1,73 @@
+module Calendar
+  module Api
+    module V1
+      class EventsController < ApplicationController
+        before_action :set_event, only: [:show, :update, :destroy]
+
+        # GET /events
+        def index
+          @events = Event.all
+
+          render json: @events
+        end
+
+        # GET /events/1
+        def show
+          render json: @event
+        end
+
+        # GET /events/search/{keyword}
+        def search
+          @events = Event.all
+
+          result_array = Array.new
+
+          @events.each_entry { |sonuc|
+            if params['keyword'].in? sonuc.title.downcase or params['keyword'].in? sonuc.description.downcase
+
+              result_array << sonuc
+            end
+          }
+
+          render json: result_array
+        end
+
+        # POST /events
+        def create
+          @event = Event.new(event_params)
+
+          if @event.save
+            render json: @event, status: :created, location: @event
+          else
+            render json: @event.errors, status: :unprocessable_entity
+          end
+        end
+
+        # PATCH/PUT /events/1
+        def update
+          if @event.update(event_params)
+            render json: @event
+          else
+            render json: @event.errors, status: :unprocessable_entity
+          end
+        end
+
+        # DELETE /events/1
+        def destroy
+          @event.destroy
+        end
+
+        private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_event
+          @event = Event.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
+        def event_params
+          params.require(:event).permit(:title, :description, :start_time, :end_time, :recurring, :reminder, :user_id)
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :calendar do
+    namespace :api do
+      namespace :v1 do
+        resources :events
+        resources :users
+        get 'events/search/:keyword', to: 'events#search'
+      end
+    end
+  end
+
   resources :events
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
